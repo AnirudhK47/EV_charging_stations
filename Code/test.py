946873b9d4e1183f6c92aeba28b5e55ac51ec4d5
Anirudh <anirudhkaushik1997@gmail.com>
2020-12-26 23:33:46 +0530

    Published Datasets

--- Code/test.py
+++ Code/test.py
@@ -1,4 +1,4 @@
 import pandas as pd
-df = pd.read_html("https://en.wikipedia.org/wiki/List_of_Namma_Metro_stations")
-dfa = df[1]
-dfa.to_csv('test.csv')
+df = pd.read_html("https://mybmtc.karnataka.gov.in/info-1/Depots/en")
+dfa = df[0]
+dfa.to_csv('depots.csv')
